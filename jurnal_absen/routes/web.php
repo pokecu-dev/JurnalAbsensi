be3d8ace2c3fd9875e558c484283d5ca5afdb551
Ririn <ririn@gmail.com>
2026-09-08 13:49:37 +0700

    feat: ui login

--- jurnal_absen/routes/web.php
+++ jurnal_absen/routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Livewire\Actions\Logout;
 
-Route::view('/', 'welcome');
+Route::view('/', 'auth.login');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
